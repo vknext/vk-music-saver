8c1094e1efb0cc85837d5fbe3b00724f0a086edb
awers24 <83694881+awers24@users.noreply.github.com>
2024-08-30 00:26:29 +0300

    New translations default.ts (Ukrainian)

--- src/langPack/Ukrainian.ts
+++ src/langPack/Ukrainian.ts
@@ -1,14 +1,14 @@
 const lang = {
-  vms_download: 'Скачать',
-  vms_loading: 'Загрузка...',
-  vms_error: 'Ошибка',
-  vms_download_cover: 'Скачать обложку',
-  vms_download_playlist: 'Скачать плейлист',
-  vms_downloading: 'Скачивается...',
-  vms_playlist_is_empty: 'Плейлист пуст.',
-  vms_playlist_not_found: 'Плейлист не найден',
-  vms_audio_not_found: 'Трек не найден',
-  vms_audio_url_not_found: 'Отсутствует ссылка на трек',
-  vms_album_cover_not_found: 'У этого альбома нет обложки'
+  vms_download: "Завантажити",
+  vms_loading: "Завантажую...",
+  vms_error: "Помилка",
+  vms_download_cover: "Завантажити обкладинку",
+  vms_download_playlist: "Завантажити плейлист",
+  vms_downloading: "Завантажується...",
+  vms_playlist_is_empty: "Плейлист порожній.",
+  vms_playlist_not_found: "Плейлист не знайдено",
+  vms_audio_not_found: "Трек не знайдено",
+  vms_audio_url_not_found: "Відсутнє посилання на трек",
+  vms_album_cover_not_found: "У цього альбому немає обкладинки"
 };
 export default lang;
