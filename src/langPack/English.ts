7c23031fe3e85dd5a614f4ca601d14d4392d0b2e
awers24 <83694881+awers24@users.noreply.github.com>
2024-08-30 00:27:05 +0300

    New Crowdin updates (#1)

---
+++ src/langPack/English.ts
@@ -0,0 +1,14 @@
+const lang = {
+  vms_download: "Download",
+  vms_loading: "Loading...",
+  vms_error: "Error",
+  vms_download_cover: "Download cover",
+  vms_download_playlist: "Download playlist",
+  vms_downloading: "Downloading...",
+  vms_playlist_is_empty: "Playlist is empty.",
+  vms_playlist_not_found: "Playlist not found",
+  vms_audio_not_found: "Track not found",
+  vms_audio_url_not_found: "Missing track link",
+  vms_album_cover_not_found: "This album doesn't have a cover"
+};
+export default lang;
---
+++ src/langPack/Ukrainian.ts
@@ -0,0 +1,14 @@
+const lang = {
+  vms_download: "Завантажити",
+  vms_loading: "Завантажую...",
+  vms_error: "Помилка",
+  vms_download_cover: "Завантажити обкладинку",
+  vms_download_playlist: "Завантажити плейлист",
+  vms_downloading: "Завантажується...",
+  vms_playlist_is_empty: "Плейлист порожній.",
+  vms_playlist_not_found: "Плейлист не знайдено",
+  vms_audio_not_found: "Трек не знайдено",
+  vms_audio_url_not_found: "Відсутнє посилання на трек",
+  vms_album_cover_not_found: "У цього альбому немає обкладинки"
+};
+export default lang;
