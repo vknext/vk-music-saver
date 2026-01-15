77629541fc6fc78e8bda01049b532556c409c749
awers24 <83694881+awers24@users.noreply.github.com>
2026-01-15 20:57:55 +0300

    New translations default.ts (Portuguese, Brazilian)

--- src/langPack/Portuguese, Brazilian.ts
+++ src/langPack/Portuguese, Brazilian.ts
@@ -17,7 +17,7 @@ const lang = {
   vms_fs_option_zip_title: "Arquivo zip",
   vms_fs_option_zip_description: 'Скачивание одним файлом. Архивация происходит в процессе скачивания без нагрузки на систему.',
   vms_fs_option_folder_title: "Diretamente na pasta",
-  vms_fs_option_folder_description: 'Вам предложен выбор места для создания новой папки, в которую загрузятся файлы напрямую на диск.',
+  vms_fs_option_folder_description: 'Вам будет предложен выбор места для создания новой папки, в которую загрузятся файлы напрямую на диск.',
   vms_fs_option_unselected_title: "Sempre perguntar",
   vms_fs_download_method_title: "Método de download de arquivos",
   vms_fs_select_zip: "Baixar",
