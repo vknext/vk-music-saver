5e52eab9fc6e1e982d6d810f5144069cf09d1e67
awers24 <83694881+awers24@users.noreply.github.com>
2026-01-18 23:32:04 +0300

    New translations default.ts (Romanian)

--- src/langPack/Romanian.ts
+++ src/langPack/Romanian.ts
@@ -48,6 +48,8 @@ const lang = {
   vms_settings_donate: "Susține dezvoltatorii",
   vms_share: "Distribuie",
   vms_clear_storage: "Resetează setările",
+  vms_clear_track_details_storage: 'Очистить кэш треков',
+  vms_clear_track_details_storage_description: 'Размер и битрейт',
   vms_created_by: "Creat de echipa {name}",
   vms_link_copied: "Link copiat",
   vms_badges_deluxe_function_warning: "Funcție disponibilă pentru abonații Prime Deluxe.",
