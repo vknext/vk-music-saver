4b0bfb386a42d776271ae36ee077ddbefa5c7d79
awers24 <83694881+awers24@users.noreply.github.com>
2026-01-18 23:32:07 +0300

    New translations default.ts (German)

--- src/langPack/German.ts
+++ src/langPack/German.ts
@@ -48,6 +48,8 @@ const lang = {
   vms_settings_donate: "Entwickler unterstützen",
   vms_share: "Teilen",
   vms_clear_storage: "Einstellungen zurücksetzen",
+  vms_clear_track_details_storage: 'Очистить кэш треков',
+  vms_clear_track_details_storage_description: 'Размер и битрейт',
   vms_created_by: "Erstellt vom Team {name}",
   vms_link_copied: "Link kopiert",
   vms_badges_deluxe_function_warning: "Funktion verfügbar für Inhaber des Prime Deluxe Abonnements.",
