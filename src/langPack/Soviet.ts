b4dd31b44d00f3e3f65be3c4d2e94eead5ddf8e9
awers24 <83694881+awers24@users.noreply.github.com>
2026-01-26 15:44:20 +0300

    New translations default.ts (Soviet)

--- src/langPack/Soviet.ts
+++ src/langPack/Soviet.ts
@@ -37,6 +37,7 @@ const lang = {
   vms_playlist_track_download_completed: '{trackName} — завершено',
   vms_playlist_download_completed: `Скачано {total}`,
   vms_tracks_plurals: ['трек', 'трека', 'треков'],
+  vms_seconds_plurals: ['секунда', 'секунды', 'секунд'],
   vms_sett_audio_convert_mode_title: 'Метод конвертации треков',
   vms_sett_audio_convert_mode_ffmpeg_desc: 'На старых системах может не работать.',
   vms_sett_audio_convert_mode_desc: 'ВКонтакте отдает треки в формате m3u8, а расширение конвертирует в привычный mp3',
@@ -77,6 +78,10 @@ const lang = {
   vms_sett_add_leading_zeros_desc: 'Например: 01, 02, 03 … вместо 1, 2, 3',
   vms_concurrent_downloads_recommendation: 'Скачивайте не больше двух плейлистов сразу, иначе браузер может зависнуть',
   vms_settings_need_reload_page: 'Перезагрузите страницу, чтобы изменения вступили в силу',
-  vms_preparing_tracklist: 'Получаем список треков...'
+  vms_preparing_tracklist: 'Получаем список треков...',
+  vms_download_manager_progress_full: '{loaded} из {total}, {speed}/с ({eta})',
+  vms_download_manager_progress_basic: '{loaded} из {total}, {speed}/с',
+  vms_size_units: ['Б', 'КБ', 'МБ', 'ГБ', 'ТБ', 'ПБ', 'ЭБ', 'ЗБ', 'ЙБ'],
+  vms_eta_almost_done: 'около секунды'
 };
 export default lang;
